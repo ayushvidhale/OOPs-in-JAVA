d9a9ab939a61230f708faf18b06c11b3491b4ca6
Ayush Vidhale <ayushvidhale099@gmail.com>
2023-01-06 22:02:35 +0530

    Lab-1-introduction-to-java

---
+++ src/Lab/Ayush.java
@@ -0,0 +1,12 @@
+package Lab;
+
+class Ayush
+{
+public static void main(String []args)
+{
+System.out.println("Ayush Vidhale");
+System.out.println("Divison: SY IT A");
+System.out.println("Roll No. 63");
+System.out.println("PRN No.1211513");
+}
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Cats are Cute!");
+        int a[][]={{1,1,1},{2,2,2},{3,3,3}};
+        int b[][]={{1,1,1},{2,2,2},{3,3,3}};
+
+
+        int c[][]=new int[3][3];
+
+        for(int i=0;i<3;i++){
+            for(int j=0;j<3;j++){
+                c[i][j]=0;
+                for(int k=0;k<3;k++)
+                {
+                    c[i][j]+=a[i][k]*b[k][j];
+                }
+                System.out.print(c[i][j]+" ");
+            }
+            System.out.println();
     }
-}
+}}
